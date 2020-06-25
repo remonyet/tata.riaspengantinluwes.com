87a9450328443ecd7f3ce4be01b93037866d338b
remonyet <67403800+remonyet@users.noreply.github.com>
2020-06-25 15:08:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light|Shadows+Into+Light+Two&amp;display=swap" rel="stylesheet"> 
+    <font size ="2"><font face="shadows into light" color="white">*admin web jangan baperan :* </font>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.75, shrink-to-fit=no">
+    <meta name="description" content="Touched By R3M0NY3T ">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Mali:400i,700i" rel="stylesheet" type="text/css">
+    <link rel="icon" href="https://l.top4top.io/p_15111p8j30.jpg">
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.1/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'>
+    <title>Touched By R3M0NY3T</title>
+    <style>
+    	html,body,.container{
+    		background: url(https://f.top4top.io/p_1564dwsqn0.jpg) no-repeat center center/cover;
+    		position: fixed;
+    		margin: auto;
+    		height: 100%;
+    		top: 0; bottom: 0; left: 0; right:0;
+    	}
+    	.page{
+    		position: absolute;
+    		margin: auto;
+    		height: 50%;
+    		top: 0; bottom: 0; left: 0; right:0;
+    	}
+    	.mek{
+    		background-color:rgba(0,0,0,0.7);
+    	}
+    	h1{
+    		transform:rotate(-12deg); 
+    		-moz-transform:rotate(-12deg); 
+    		-webkit-transform:rotate(-12deg); 
+    		-o-transform:rotate(-12deg);
+    		font-family: "Mali";
+    		font-size: 2em;
+    		font-weight: bold;
+    		margin-bottom: 15%;
+    	}
+    	h5{
+    		font-family: "Mali";
+    	}
+    	.logo{
+    		text-decoration: none;
+    		color: white;
+    		font-size: 22px;
+    		margin: 10px;
+    	}
+    	.logo:hover{
+    		text-decoration: none;
+    		color: white;
+    		cursor: pointer;
+    	}
+    	.hastag{
+    		text-decoration: none;
+    		color: white;
+    	}
+    </style>
+  </head>
+  <body>
+  	
+    <div class="container">
+    	<div class="page">
+    		<h1 class="text-center text-white"> Touched By R3M0NY3T</h1>
+    		
+    		<div class="mek">
+    		<h5 class="text-center text-white mb-3"><hr><strong>#FR13NDS 404 INDO NET</strong><br><hr>{Message}<br>Dunia terbalik. dimana mastah merendah , sedangkan para nob belagak seperti mastah<br><hr></div><marquee class="mr-5 ml-5"><a class="hastag" href=</a>| Khintil - Lo@ding -./Lucy - S27KUN - Lala - Noobie - R0sX_010 -./Mark  - F3RGUS0 - Nox - Skyzhoo - Semvak - Palestina - queen lia - laylatul modar - ./Scary - Faney - hildaay28 - mrs harley - naraxploit - razor - IpanKun - VenoRyan - RAhack - Uchia°C4D2Z5 - yemima - x-pone - xren - exsel - Darknet_Seva - Rival gans - Neoidnf - Salwa - Xwhynot - Senopati☆Leon - Mr.5umm3r_l19h7 - MR.CRUP-(ARIF) | </marquee></h5>
+    		 
+    		<center>
+    		
+    		 
+    		<br><br><center><font color="red" size="4">Touch Me  :</font><br><a href='https://api.whatsapp.com/send?phone=+62895808608168&text=Hello Sir,You Gans'> <img alt='WhatsApp' src='https://www.freepngimg.com/thumb/whatsapp/77193-zubees-icons-foods-computer-halal-logo-whatsapp.png ' width='40' height='40'></a>  <a href='https://youtu.be/Shs4xODrSYo'> <img alt='Youtube' src='https://www.upload.ee/image/11226706/images__5_-removebg-preview.png' width='40' height='40'></a> </footer>
+    		<br><br><br>
+    		<a class="logo" href="">
+    			<i class="fab fa-whatsapp"></i>
+    		</a>
+    		<a class="logo" href="">
+    			<i class="fab fa-hackerrank"></i>
+    		</a>
+    		<a class="logo" href="">
+    			<i class="fab fa-github"></i>
+    		</a>
+    		<a class="logo" href="">
+    			<i class="fas fa-globe"></i>
+    		</a>
+    		<a class="logo" href="">
+    			<i class="fab fa-blogger"></i>
+    		</a>
+    		
+    		</center>
+    	</div>
+    </div>
+
+<iframe width="0" height="0" src="https://i.top4top.io/m_15809uaez0.mp3" frameborder="0" allowfullscreen></iframe>
+  </body>
+</html>
